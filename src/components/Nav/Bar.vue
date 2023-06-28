ceac0467ccdbbfc2d89c152dda351a8c2c89f25d
Paarth Kukreja <paarthkukreja@outlook.com>
2023-06-28 16:45:53 +0800

    very basic setup done

---
+++ src/components/Nav/Bar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div id="paarth-logo">Logo</div>
+    <nav-link
+      class="nav-links interactable"
+      v-for="data in navLinkData"
+      :to="data.to"
+      :key="data"
+    >
+      {{ data.display }}
+    </nav-link>
+  </div>
+</template>
+<script setup>
+import NavLink from "./Link.vue";
+</script>
+<script>
+export default {
+  data() {
+    return {
+      navLinkData: [
+        {
+          to: "#about",
+          display: "About Me",
+        },
+        {
+          to: "#projects",
+          display: "Projects",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.container {
+  position: fixed;
+  width: 99vw;
+  background-color: rgba(0, 0, 0, 0);
+  height: auto;
+  text-align: right;
+  padding: 7px;
+}
+
+#paarth-logo {
+  padding: 10px;
+  float: left;
+  color: black;
+  font-size: 25px;
+}
+</style>
---
+++ src/components/Nav/Link.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="nav-link-container">
+    <a :href="to">
+      <button>
+        <slot />
+      </button>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["to"],
+};
+</script>
+
+<style scoped>
+.nav-link-container {
+  display: inline-block;
+  height: 100%;
+  list-style-type: none;
+  text-align: center;
+  margin-top: 3px;
+
+  margin-left: 17px;
+  border-radius: 30px;
+  border: 1px solid;
+  transition: border 0.1s;
+}
+.nav-link-container:hover {
+  transition: border 0.1s;
+  border: 1px #3299cd dashed;
+}
+
+.nav-link-container:hover button {
+  background-color: white;
+}
+button {
+  /* filter: invert(); */
+  display: inline-block;
+  padding-top: 15px !important;
+  padding: 14px;
+  border-radius: 30px;
+  border: none;
+  text-transform: uppercase;
+  transition: all 0.25s;
+  background-color: #84e1d2;
+  pointer-events: none;
+}
+</style>
