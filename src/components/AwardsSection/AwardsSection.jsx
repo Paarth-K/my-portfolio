4107cfc7c01ee84eb63a85c2df89f24211e68386
Paarth Kukreja <paarthkukreja@outlook.com>
2023-07-08 23:42:15 +0800

    fixed fonts, and added awards section

---
+++ src/components/AwardsSection/AwardsSection.jsx
@@ -0,0 +1,29 @@
+import Styles from "./AwardsSection.module.scss";
+export default function ProjectsSection() {
+  const projects = [
+    {
+      link: "https://www.paarthkukrejadofe.eu.org",
+      desc: "I made this project with Nuxt3, for the Skills section of the Duke of Edinburgh Award",
+      image: "/projects/dofeweb_dark.png",
+      imgalt: "DofE Website",
+    },
+    {
+      link: "https://colab.research.google.com/drive/1rApehuk87KAJgl2RmoEk3X2eJc9l3BoW?usp=sharing",
+      desc: "I made this Data Science Project with Python, for the Silicon Valley Competition by Byju's",
+      image: "/projects/siliconvc.png",
+      imgalt: "SVC Byju's",
+    },
+  ];
+  return (
+    <div className={Styles.main} id="projects">
+      <div className={Styles.project}>
+        <div className={Styles.projectTitle}>
+          <span>Awards I've Won</span>
+        </div>
+        <div className={Styles.projectGrid}>
+          <div className={Styles.projectContent}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
