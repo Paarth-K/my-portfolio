c7c39db99842307b6ab40fdabbeeb89bd8bd4ba8
Paarth Kukreja <paarthkukreja@outlook.com>
2023-09-27 22:00:09 +0400

    re-ordering projects

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -3,6 +3,13 @@ import ProjectTile from "./ProjectTile";
 export default function ProjectsSection() {
   const projects = [
     {
+      link: "https://internshipfinder.vercel.app",
+      desc: "This project is being made with a few of my friends and uses Next.js. It aims to help high school students find internships.",
+      image: "projects/internshipfinder_hdwip_preview.png",
+      imgalt: "Internship Finder Website",
+      isWIP: true,
+    },
+    {
       link: "https://dofe.paarthkukreja.com",
       desc: "I made this project with Nuxt3, for the Skills section of the Duke of Edinburgh Award.",
       image: "/projects/dofeweb_dark.png",
@@ -13,13 +20,6 @@ export default function ProjectsSection() {
       desc: "I made this Data Science Project with Python, for the Silicon Valley Competition by Byju's.",
       image: "/projects/SiliconVCNew.png",
       imgalt: "SVC Byju's",
-    },
-    {
-      link: "https://internshipfinder.vercel.app",
-      desc: "This project is being made with a few of my friends and uses Next.js. It aims to help high school students find internships.",
-      image: "projects/internshipfinder_hdwip_preview.png",
-      imgalt: "Internship Finder Website",
-      isWIP: true,
     },
   ];
   return (
