ede1d6b7a4005b33a5bdeaf194fa52bac5c91a39
Paarth-K <paarthkukreja@outlook.com>
2025-09-08 21:39:45 +0400

    using webp optimized images

--- src/components/Base/Image.jsx
+++ src/components/Base/Image.jsx
@@ -10,7 +10,7 @@ export default function Image({
       decoding="async"
       // loading="lazy"
       draggable={draggable}
-      src={`https://cdn.paarthkukreja.com/?url=https://res.cloudinary.com/dgo87obt6/image/upload/${transformations}/portfolio-assets/${src}&il&af`}
+      src={`https://cdn.paarthkukreja.com/?url=https://res.cloudinary.com/dgo87obt6/image/upload/${transformations}/portfolio-assets/${src}&output=webp`}
       alt={alt}
       {...other}
     />
