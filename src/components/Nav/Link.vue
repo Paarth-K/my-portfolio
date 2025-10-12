385fd04e4d0be5e3a74f533584a9a9bd2dcb4d8e
Paarth-K <paarthkukreja@outlook.com>
2025-10-12 22:10:05 +0400

    package updates and 3d style nav link buttons

--- src/components/Nav/Link.vue
+++ src/components/Nav/Link.vue
@@ -24,14 +24,25 @@ export default {
   height: 100%;
   list-style-type: none;
   text-align: center;
-
+  cursor: pointer;
   border-radius: 30px;
-  border: 1px solid;
-  transition: border 0.1s;
+  border: 1px solid #1e1d1d;
+  box-shadow: 0px 0px 0px #1e1d1d;
+  transition:
+    box-shadow 0.1s,
+    transform 0.1s;
 }
 .nav-link-container:hover {
-  transition: border 0.1s;
-  border: 1px var(--primary) dashed;
+  box-shadow: 4px 4px 0px #1e1d1d;
+  transform: translate(-2px, -2px);
+}
+
+.nav-link-container:active {
+  transition:
+    box-shadow 0.05s,
+    transform 0.05s;
+  box-shadow: 0px 0px 0px #1e1d1d;
+  transform: translate(0px, 0px);
 }
 @media (max-width: 385px) {
   .nav-link-container {
@@ -47,6 +58,7 @@ export default {
   display: inline-block;
   padding: 14px;
   padding-top: 15px !important;
+  cursor: pointer;
   font-size: 10pt;
   border-radius: 30px;
   border: none;
